cbb109fb7efb21860213a2280e3cc7a4141a454f
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-03 01:41:03 +0300

    Task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* main - To print to standard error
-
-* Description: A program that prints output to standard error
-* Return: 1
-*/
+ * main - To print to standard error
+ * Description: A program that prints output to standard error
+ * Return: 1
+ */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19
+\n");
 return (1);
 }
