44a9c45cf12cc127f191afc5d4a6c5bdf287100c
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-03 01:48:04 +0300

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main -program that uses printf
- * Description: prints exactly with proper grammar, but the outcome is a piece  of art.
+ * main - A program that uses printf
+ * Description: Prints with proper grammar, but the outcome is a piece of art,
  * Return: 0
  */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
