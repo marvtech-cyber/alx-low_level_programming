d522cc7efd0930d065dda9f469391dea90ea2706
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-03 23:23:24 +0300

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *  main - Entry point
+ * main - Entry point
  *
- *  Return: returns 0 (Success)
+ * Return: returns 0 (Success)
  */
 int main(void)
 {
