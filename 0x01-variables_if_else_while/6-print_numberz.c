6a0c7c94175f76ac1cbe614da1e2854b22cf4727
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-06-04 11:58:45 -0700

    print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - initialization point
+ * Return: 0
+ */
+int main(void)
+{
+	int m;
+
+	for (m = '0'; m <= '9'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
