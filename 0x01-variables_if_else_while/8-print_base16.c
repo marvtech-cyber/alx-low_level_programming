a3d29ad4e728f730fc20b42cd5d125adfeb415e5
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-06-04 12:41:43 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - initialization point
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+		printchar(a);
+
+	for(a = 'a'; a <= 'f'; a++)
+		printchar(a);
+
+	printchar('\n');
+	return (0);
+}
