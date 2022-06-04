642fa720173a7cd19ce8da135d266af320f365e0
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-06-04 13:17:39 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - initialization point
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		if (a != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
