550714a81621a1faa4fab70f79a29925b478e930
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-06-04 07:27:44 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Starting point
+ *
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+	n = 0;
+	while(n < 10)
+	{
+		printf("%d\n". n);
+	}
+	return (0);
