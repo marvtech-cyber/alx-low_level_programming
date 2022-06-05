fb283e509e975dc2b47ebfa319c3a2fabbbb7f76
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-05 12:52:59 +0300

    fixed for error

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,17 +14,17 @@ int main(void)
 	for (a = 0; a < 8; a++)
 	{
 		for (b = 0 + 1; b < 9; b++)
-		{
-			putchar((a % 10) + '0');
-			putchar((b % 10) + '0');
-			putchar((c % 10) + '0');
-			if (a == 7 && b == 8 && c ==9)
-				continue;
-			putchar(',');
-			putchar(' ');
-		}
+			for (c = b + 1; c < 10; c++)
+			{
+				putchar((a % 10) + '0');
+				putchar((b % 10) + '0');
+				putchar((c % 10) + '0');
+				if (a == 7 && b == 8 && c ==9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
