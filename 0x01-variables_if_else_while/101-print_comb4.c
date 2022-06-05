38495d44cdbb933d8395721e1d4b6c05e3846bd2
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-05 12:54:56 +0300

    closed second for

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,6 +14,7 @@ int main(void)
 	for (a = 0; a < 8; a++)
 	{
 		for (b = 0 + 1; b < 9; b++)
+		{
 			for (c = b + 1; c < 10; c++)
 			{
 				putchar((a % 10) + '0');
@@ -24,6 +25,7 @@ int main(void)
 				putchar(',');
 				putchar(' ');
 			}
+		}
 	}
 	putchar('\n');
 	return (0);
