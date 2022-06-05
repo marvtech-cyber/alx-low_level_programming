d0114ec99d9feb92eacd30ef0d01500662b3f7ad
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-06-05 03:31:39 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - initialization point 
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 99; a++)
+	{
+		for (b = a + 1; b < 100; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
