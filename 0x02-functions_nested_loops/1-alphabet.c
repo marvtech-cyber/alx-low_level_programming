0b43ad4cc775559a296cd33444f1d44c38726393
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-07 23:51:09 +0300

    fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
  * main - prints alphabet from a to z in lowercase
  * Return: always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char i;
-	
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
