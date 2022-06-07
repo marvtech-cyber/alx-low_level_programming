9566e9b82754e68eda6e953cd32f32e124004eda
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-06-07 13:30:59 -0700

    lowercase alphabet task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet from a to z in lowercase
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	if (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+	return (0);	
