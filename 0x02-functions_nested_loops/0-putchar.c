f90d5245b1732e75b7bdf372332ae81c183dcf15
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-07 22:13:49 +0300

    added new line and main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - a program that outputs _putchar
  * Return: always 0 (Success)
  */
 int main(void)
 {
-	putchar("_putchar");
+	putchar("_putchar\n");
 	return (0);
 }
