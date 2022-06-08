34cb28f26fe017244d5ecda8822993ef1f819692
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-08 19:47:30 +0300

    multiply table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,9 +6,9 @@
  */
 void print_times_table(int n)
 {
-	int a, b, c;
-
-	if (n >= 0 && n <= 15)
+	int a, b, c:
+	
+	if (n >=0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
 		{
@@ -18,26 +18,29 @@ void print_times_table(int n)
 				if (b == 0)
 				{
 					_putchar(c + '0');
-				} else if (c < 10 && b != 0)
+				}
+				else if (c < 10 && b != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(c + '0');
-				} else if (c >= 10 && c < 100)
+					_putchar(' ');
+				}
+				else if (c >= 10 && c < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');
-				} else if (c >= 100)
+					_putchar((c / 100) + '0');
+					_putchar((c % 10 ) + '0');
+				}
+				else if (c >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((c / 100) + '0');
-					_putchar(((c / 10) % 10) + '0');
+					_putchar((c /10) % 10 ) + '0');
 					_putchar((c % 10) + '0');
 				}
 			}
