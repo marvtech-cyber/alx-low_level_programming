91782c29491df17deffd72b673d669bb5b875f4a
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-08 16:43:23 +0300

    program for n

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,26 @@
-#include
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_to_98 - prints all natural numbers from n to 98
+ * @n: parameter
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
+}
