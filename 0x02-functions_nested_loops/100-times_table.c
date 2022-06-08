1bd6a24691bd4f81e3413b0c886c39bcdcc0e3b4
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-08 20:07:32 +0300

    fixed second printchar

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,7 +32,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((c / 100) + '0');
+					_putchar((c / 10) + '0');
 					_putchar((c % 10 ) + '0');
 				}
 				else if (c >= 100)
