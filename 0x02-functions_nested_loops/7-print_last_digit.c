340e1fbb67f9dde10c8009804692985c56fa904d
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-08 15:23:04 +0300

    last integer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
 /**
+ * print_last_digit - entry point
  *
- *
- *
+ * Description: finds the last didit of an integer
+ * Return: the value of the last digit
  */
+int print_last_digit(int)
+{
+  int digit;
+  
+  i = digit % 10;
+  return (i);
+}
