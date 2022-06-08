2e62ecb14f005515e8bb534cd66c4a12cc801b54
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-06-07 22:38:47 -0700

    program that

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet ten times
+ */
+void print_alphabet_x10(void)
+{
+	int 1 = 0;
+	while (i <= 10)
+	{
+		char i;
+
+		for (i = 'a'; i <= 'z'; i++)
+			putchar("%s\n", i);
+		putchar("\n");
+	}
