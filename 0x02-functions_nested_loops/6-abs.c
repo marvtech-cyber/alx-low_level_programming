6d6aa0d918cd5ad71480d13453364625211d676d
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-08 15:17:35 +0300

    added return

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,6 +6,7 @@
  * @n: parameter integer to find absolute value
  *
  * Description: checks the absolute value of an integer
+ * Return: n if positive value or -n for negative
  */
 int _abs(int n)
 {
