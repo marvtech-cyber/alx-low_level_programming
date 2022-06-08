e623a876d373f8c678045345749b57595544c168
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-08 12:15:41 +0300

    added parenthesis and fixed @c parameter description

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,14 @@
 
 /**
  * _islower - checks if character is lower
- @c: parameter for checking whether is lower
+ * @c: parameter for checking whether is lower
  *
  * return: 1 if c is lowercase otherwise 0
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return 1;
+		return (1);
 	else
 		return (0);
 }
