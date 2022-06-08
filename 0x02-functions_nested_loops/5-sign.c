4fccb7e4b095270afb855a822367402525321cdd
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-08 13:06:41 +0300

    fixed putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,13 +15,15 @@ int print_sign(int n)
 		_putchar("+");
 		return (1);
 	}
-	elseif (n == 0)
+	else if (n == 0)
 	{
 		_putchar("0");
+		return (0);
 	}
 	else
 	{
-		_putchar("-);
+		_putchar("-");
 		return (-1);
 	}
+	_putchar("\n");
 }
