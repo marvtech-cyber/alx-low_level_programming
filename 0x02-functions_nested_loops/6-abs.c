1f18a055b8f3162ae2b8e3105a7d7b4306636ef5
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-08 15:03:18 +0300

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,21 @@
-nclude "main.h"
+#include "main.h"
+#include <stdlib.h>
 
 /**
- *
- *
- *
+ * _abs - entry point
+ * @i: parameter integer to find absolute value
+ * 
+ * Description: checks the absolute value of an integer
+ * Returns: The absolute value 
  */
+int _abs(int i)
+{
+  if (i >= 0)
+  {
+    return (i);
+  }
+  else
+    i = i * -1
+    return (i);
+}
+    
