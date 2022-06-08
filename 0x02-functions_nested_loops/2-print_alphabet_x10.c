8b759e81f7b22603ff35acd651c1d11980082a90
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-08 09:11:03 +0300

    declared variable x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,12 @@
 void print_alphabet_x10(void)
 {
 	int i = 0;
+	char x;
 	
 	while (i <= 10)
 	{
-		char i;
-
-		for (i = 'a'; i <= 'z'; i++)
-			putchar("%s\n", i);
+		for (x = 'a'; x <= 'z'; x++)
+			putchar(x);
 		putchar("\n");
 		i++;
 	}
