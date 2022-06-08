b4130e9b189b3c5b4d3a4cf9368837b9581e3112
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-08 19:17:30 +0300

    fixed program whitespace error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-
+	
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -18,7 +18,8 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
+				} 
+				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
