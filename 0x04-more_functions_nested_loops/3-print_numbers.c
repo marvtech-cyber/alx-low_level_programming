fad8628ed2b949c127674db5f228b5841597aaf7
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-06-09 12:42:43 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * Return: void
+ */
+void print_numbers(void)
+{
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n")
+}
