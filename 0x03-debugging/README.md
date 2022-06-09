c7d3a1e9274d4e8032e6e13367a794d88e64c558
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-06-09 02:50:00 -0700

    debugging

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This project is meant to learn the different ways and techniques of debbuging.
---
+++ 0x03-debugging/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	I = 98;
+	positive_or_negative(1);
+
+	return (0);
+}
