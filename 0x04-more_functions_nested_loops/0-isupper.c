3924e8abc9afda01d695fabcabd4d8e547638f14
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-06-09 11:01:13 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: parameter
+ *
+ * Return: 1 (success) otherwise 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c) == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0-isupper a function that checks for uppercase character
