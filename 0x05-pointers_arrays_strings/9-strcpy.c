24308ca1f70f04883c7f2c79c06b8adfe038a99c
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-06-13 05:30:46 -0700

    copies string pointed

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * -strcpy - entry point
+ *  @dest: string pointer
+ *  @src: buffer pointer
+ *  
+ *  Description: copies string pointed by src
+ *  Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		dest[i] = src[i];
+
+		if (src[i] == '\0')
+		{
+			return (dest);
+			i++;
+		}
+	}
+	return (dest);
+}
