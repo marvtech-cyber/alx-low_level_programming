4ebf2f7d91d7240af12a20fa2dd38126ad45929f
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-06-13 05:10:19 -0700

    prints array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * print_array - entry point
+ * @a: integer pointer parameter
+ * @n: integer parameter
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
