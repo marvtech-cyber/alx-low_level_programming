709211eb47ae6f31fa58e0425651b4ee1341de4a
marvin nyarenchi <80676201+marvtech-cyber@users.noreply.github.com>
2022-06-15 20:25:01 +0300

    fixed whitespace

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,13 +4,13 @@
  *_strcat - function that concatenates two strings.
  *@dest: pointer to destination string.
  *@src: pointer to source string.
- *
  *Return: pointer to destination string.
  */
 char *_strcat(char *dest, char *src)
 {
 
 	int length = 0,j;
+	
 	while (dest[length] != '\0')
 	{
 		length++;
