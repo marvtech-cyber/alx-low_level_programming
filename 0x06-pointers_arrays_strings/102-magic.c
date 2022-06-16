dd393066856e374b8837e4ab2fb94607c5869bb5
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-06-16 01:34:44 -0700

    added main description

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/**
+* main - entry point
+*/
 
 int main(void)
 {
@@ -16,7 +19,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 5) = 98;/* pointer incremental*/
+  *(p + 5) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
