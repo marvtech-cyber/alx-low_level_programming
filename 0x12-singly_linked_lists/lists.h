2acdef84edbb8345c9e9797cfe3e8d34b55a4403
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-07-14 07:20:12 -0700

    task 2 add function that adds node at beginning

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,5 +19,6 @@ typedef struct singly_linked_list
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
